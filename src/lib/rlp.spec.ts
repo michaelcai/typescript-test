3e4ff41b8119ac30fc1e0e799ba9858ebeb05fc6
caizhimai <czm.michael@gmail.com>
2018-04-27 18:30:45 +0800

    fix

--- src/lib/rlp.spec.ts
+++ src/lib/rlp.spec.ts
@@ -25,3 +25,10 @@ test('lib/rlp/encode', t => {
   t.is(rlp.encode('0x00112233445566778899aabbccddeeff'), '0x9000112233445566778899aabbccddeeff')
   t.is(rlp.encode(["0x00112233", "0x00", "0x44", "0x55", "0xf0", "0xff", ["0x66"], ["0x77", "0x88"], "0x", "0x", "0x99aabb"]), '0xd8840011223300445581f081ffc166c377818880808399aabb')
 })
+
+test('lib/rlp/decode', t => {
+  t.deepEqual(rlp.decode('0x83212412'), '0x212412')
+  t.deepEqual(rlp.decode('0x9000112233445566778899aabbccddeeff'), '0x00112233445566778899aabbccddeeff')
+  t.deepEqual(rlp.decode('0xd8840011223300445581f081ffc166c377818880808399aabb'), ["0x00112233", "0x00", "0x44", "0x55", "0xf0", "0xff", ["0x66"], ["0x77", "0x88"], "0x", "0x", "0x99aabb"])
+  t.deepEqual(rlp.decode('0xd8840011223300445581f081ffc166c37781888080839aabb'), [])
+})
--- src/lib/rlp.ts
+++ src/lib/rlp.ts
@@ -9,39 +9,85 @@
 // | 248 to 255 | HEX(length_of_length_of_node + 128 + 55) + HEX(length_of_node) + HEX(node) |
 
 export const padEven = (str: string): string => {
-  return str.length % 2 === 0 ? str : `0${str}`
+	return str.length % 2 === 0 ? str : `0${str}`
 }
 
 export const uint = (num: number): string => padEven(num.toString(16))
 
 export const length = (len: number, add: number): string => {
-  return len < 56
-    ? uint(add + len)
-    : uint(add + uint(len).length / 2 + 55) + uint(len)
+	return len < 56 ? uint(add + len) : uint(add + uint(len).length / 2 + 55) + uint(len)
 }
 
 export const dataTree = (tree: string | any[]): string => {
-  if (typeof tree === 'string') {
-    const hex = tree.slice(2)
-    const pre =
-      hex.length !== 2 || hex >= '80' ? length(hex.length / 2, 128) : ''
-    return pre + hex
-  } else {
-    const hex = tree.map(dataTree).join('')
-    const pre = length(hex.length / 2, 192)
-    return pre + hex
-  }
+	if (typeof tree === 'string') {
+		const hex = tree.slice(2)
+		const pre = hex.length !== 2 || hex >= '80' ? length(hex.length / 2, 128) : ''
+		return pre + hex
+	} else {
+		const hex = tree.map(dataTree).join('')
+		const pre = length(hex.length / 2, 192)
+		return pre + hex
+	}
 }
 
 const encode = (tree: string | any[]): string => `0x${dataTree(tree)}`
 
-const parseLength = (hex: string, index: number) => {
-  const len = parseInt(hex.slice(index, index + 2), 16) % 64
-  return len < 56
-    ? len
-    : parseInt(hex.slice(index + 2, index + 2 + (len - 55) * 2), 16)
+export interface DecodeInterface {
+	len: number
+	tree: string | any[]
+}
+
+const parseLength = (hex: string): number => {
+	const len = parseInt(hex.slice(0, 2), 16) % 64
+	return len < 56 ? len : parseInt(hex.slice(0, (len - 55) * 2), 16)
+}
+
+const parseHex = (hex: string): DecodeInterface => {
+  const len = parseLength(hex)
+	if ((hex.length - 2) / 2 < len) {
+		throw ''
+	}
+	return { len, tree: `0x${hex.slice(2, len * 2 + 2)}` }
+}
+
+const parseList = (hex: string): DecodeInterface => {
+	const resultTree = []
+	const len = parseLength(hex)
+	let nowLen = 0
+	let nowHex = hex
+	while (nowLen < len) {
+		nowHex = nowHex.slice(nowLen * 2 + 2)
+		const { len: thisLen, tree } = parseTree(hex.slice(nowLen * 2 + 2))
+		nowLen += thisLen + 1
+		resultTree.push(tree)
+	}
+	return { len, tree: resultTree }
+}
+
+const parseTree = (hex: string): DecodeInterface => {
+	if (hex.length === 0) {
+		throw ''
+	}
+	const head = hex.slice(0, 2)
+	if (head < '80') {
+		return { len: 0, tree: `0x${head}` }
+	} else if (head < 'c0') {
+		return parseHex(hex)
+	} else {
+		return parseList(hex)
+	}
+}
+
+const decode = (hex: string) => {
+	try {
+		const { tree } = parseTree(hex.slice(2))
+		return tree
+	} catch (_) {
+		return []
+	}
 }
 
 export default {
-  encode
+	decode,
+	encode
 }
